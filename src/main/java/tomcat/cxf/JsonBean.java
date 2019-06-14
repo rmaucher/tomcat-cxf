0de80fb307e7c8896f4a3640846ebddb7be98eb1
remm <remm@apache.org>
2019-06-14 17:02:01 +0200

    Add web-fragment and minor cleanup

--- src/main/java/tomcat/cxf/JsonBean.java
+++ src/main/java/tomcat/cxf/JsonBean.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.tomcat.cxf;
+package tomcat.cxf;
 
 import javax.enterprise.context.Dependent;
 import javax.ws.rs.Produces;
